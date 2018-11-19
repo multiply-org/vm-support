7e3e7cf844d79707e8b430befb3b230f79471464
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-11-19 16:31:05 +0100

    added to init

--- vm_support/__init__.py
+++ vm_support/__init__.py
@@ -1,3 +1,3 @@
 from .version import __version__
 from .updates import update
-from .utils import create_config_file, get_working_dir, set_earth_data_authentication
+from .utils import create_config_file, get_working_dir, set_earth_data_authentication, set_permissions

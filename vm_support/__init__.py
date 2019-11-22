ad41dc9d612d133061e92c6120a8ebcaef327181
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-11-22 16:00:36 +0100

    edited init

--- vm_support/__init__.py
+++ vm_support/__init__.py
@@ -2,4 +2,4 @@ from .version import __version__
 from .mundi_aux_data_provider import MundiAuxDataProvider, MundiAuxDataProviderCreator
 from .sym_linker import create_sym_links
 from .utils import create_config_file, create_sar_config_file, get_working_dir, set_earth_data_authentication, \
-    set_permissions, set_up_data_stores
+    set_permissions, set_up_data_stores, set_mundi_authentication

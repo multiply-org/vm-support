73b8071b2533ec634b9f44e4cbd73cb7e2b1dd17
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-12-20 15:28:29 +0100

    edited init

--- vm_support/tools/__init__.py
+++ vm_support/tools/__init__.py
@@ -1,3 +1,3 @@
 from .tools import create_dir, put_data, get_static_data, get_dynamic_data, get_priors, get_priors_from_config_file, \
     get_priors_old, InvTransformation, preprocess, preprocess_s2, infer, infer3, infer_new, Plot_SRDS, Plot_PRIORS, \
-    Plot_TRAITS, Plot_Transformation, Plot_TRAIT_evolution
+    Plot_TRAITS, Plot_Transformation, Plot_TRAIT_evolution, Read_TraitPriors2, Classify_PRIORS3
